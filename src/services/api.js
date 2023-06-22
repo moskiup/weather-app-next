eb32099d8ea9b0410f3c4beba144cf2c193e9f1d
moskiup <moskiup@gmail.com>
2023-06-22 17:56:10 -0600

    remove console.log

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
-const BASE_URL = 'http://api.openweathermap.org/data/2.5/';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 const API_KEY = process.env.NEXT_PUBLIC_APIKEY;
-console.log({ API_KEY });
 import { respuesta } from './response';
 
 function urlIcon(code) {
