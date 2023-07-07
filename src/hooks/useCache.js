9e8fdd30f306636efff51e84934535ecef7a133b
moskiup <moskiup@gmail.com>
2023-07-07 16:14:54 -0600

    adding cache

---
+++ src/hooks/useCache.js
@@ -0,0 +1,76 @@
+const cacheStore = {};
+const defaultNamespace = Symbol('defaultNamespace');
+
+//expiresAt is expired if it is set and in the past
+function expired(expiresAt) {
+  return expiresAt && expiresAt < Date.now();
+}
+
+//Pull this out of useNamespace
+function getNamedCache(namespace){
+  if(!cacheStore[namespace]) {
+      cacheStore[namespace] = new Map();
+  }
+  
+  return cacheStore[namespace];
+}
+
+function useNamespace(namespace) {
+  const namedCache = getNamedCache(namespace);
+  
+  function get(key) {
+    //destructure get so we have the value and expiresAt
+    //the || {} protects us if our key doesn't exist
+    const { value, expiresAt } = namedCache.get(key) || {};
+    
+    //if the key is expired return undefined
+    //It is like it never existed
+    if (expired(expiresAt)) {
+      //let's be good citizens and remove the key 
+      remove(key);
+      return undefined;
+    }
+
+    return value;
+  }
+  
+  //add an optional timeout parameter; if not set, value will never expire
+  //timeout is in milliseconds
+  function set(key, value, timeout) {
+    //if timeout is passed in then set expiresAt to the current time plus the timeout
+    const expiresAt = timeout ? Date.now() + timeout : null;
+    namedCache.set(key, { value, expiresAt });
+  }
+  
+  //see how much time is left until the key expires
+  function timeTillExpires(key) {
+    const { expiresAt } = namedCache.get(key) || {};
+    //if there is no expiresAt or the key doesn't exist
+    if(!expiresAt) { 
+      return null;
+    }
+    
+    //if the key has already expired return 0
+    if(expired(expiresAt)) {
+      return 0;
+    }
+    
+    return expiresAt - Date.now();
+  }
+
+  function remove(key) {
+    namedCache.delete(key);
+  }
+  
+  function removeAll() {
+    namedCache.clear();
+  }
+  
+  return {get, set, timeTillExpires, remove, removeAll};
+}
+
+const {get, set, timeTillExpires, remove} = useNamespace(defaultNamespace);
+
+//We don't expose removeAll on the defaultNamespace. 
+//We only allow remove all on custom namespaces
+module.exports = {get, set, timeTillExpires, remove, useNamespace}
--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import { getDailyData, getDayData, getDayHourData, getLangLong } from '@/services/api';
 import { useLocalStorage } from './useLocalStore';
+import { get as getcache, set as setcache } from './useCache';
+
+let cities_latlon = {};
+const timecache = 5*3600;
 
 export function useWeather() {
   const [today, setToday] = useState({});
@@ -9,7 +13,6 @@ export function useWeather() {
   const [error, setError] = useState(null);
   const [isMetric, setIsMetric] = useState(true);
   const [lisMetric, lsetIsMetric] = useLocalStorage('metric', true);
-  // const [city, _setCity] = useState('');
 
   const [isLoading, setLoading] = useState(true);
   const [gps, setGps] = useState({ lat: 19.4285, lon: -99.1277 });
@@ -23,10 +26,10 @@ export function useWeather() {
   ]);
   let city = '';
 
+
   //LOAD CONFIGURATION
   useEffect(() => {
     setCities(lcities);
-    city = lcities[0];
     setCity(lcities[0], true);
     setIsMetric(lisMetric);
   }, []);
@@ -36,28 +39,48 @@ export function useWeather() {
       try {
         setLoading(true);
 
-        const prom1 = getDayData(gps.lat, gps.lon, isMetric);
-        const prom2 = getDayHourData(gps.lat, gps.lon);
-        const prom3 = getDailyData(gps.lat, gps.lon);
+        const cache_today = `t${gps.lat},${gps.lon}`;
+        const cache_daily = `d${gps.lat},${gps.lon}`;
+        const cache_hourly = `h${gps.lat},${gps.lon}`;
+        
+        if (getcache(cache_today) === undefined) {
+          const prom1 = getDayData(gps.lat, gps.lon, isMetric);
+          const prom2 = getDayHourData(gps.lat, gps.lon);
+          const prom3 = getDailyData(gps.lat, gps.lon);
 
-        Promise.all([prom1, prom2, prom3])
+          
+          Promise.all([prom1, prom2, prom3])
           .then((values) => {
+            if (getcache(cache_today) === undefined) setcache(cache_today, values[0], timecache);
+            if (getcache(cache_hourly) === undefined) setcache(cache_hourly, values[1], timecache);
+            if (getcache(cache_daily) === undefined) setcache(cache_daily, values[2], timecache);
+
             setToday(values[0]);
-            setHourly(values[1]);
-            setDaily(values[2]);
-            setLoading(false);
-          })
-          .catch((error) => {
-            throw error;
-          });
+              setHourly(values[1]);
+              setDaily(values[2]);
+              setLoading(false);
+            })
+            .catch((error) => {
+              throw error;
+            });
+        }
+        else {
+          setToday(getcache(cache_today));
+          setHourly(getcache(cache_hourly));
+          setDaily(getcache(cache_daily));
+          setLoading(false);
+
+        } 
       } catch (err) {
         setError(err);
+        setLoading(false);
+
         console.log(err);
       }
     };
 
     today_data();
-  }, [gps, isMetric]);
+  }, [gps]);
 
   function changeUnits(val) {
     setIsMetric(val);
@@ -70,17 +93,19 @@ export function useWeather() {
     try {
       if (_city === city && !fisrtTime) return;
       if (_city !== '' || fisrtTime) {
-        const data3 = await getLangLong(_city);
-
-        if (data3.length === 0) throw new Error('City doesnt exist');
-
         if (cities[0] !== _city) {
           if (!fisrtTime) {
             setCities((prev) => [_city, ...prev.filter((x) => x !== _city).slice(0, 4)]);
             lsetCities([_city, ...cities.filter((x) => x !== _city).slice(0, 4)]);
           }
         }
-        setGps({ lat: data3[0], lon: data3[1] });
+        if (cities_latlon[_city] === undefined) {
+          const data3 = await getLangLong(_city);
+          if (data3.length === 0) throw new Error('City doesnt exist');
+          cities_latlon[_city] = { lat: data3[0], lon: data3[1] };
+        }
+
+        setGps(cities_latlon[_city]);
         city = _city;
       }
     } catch (error) {
